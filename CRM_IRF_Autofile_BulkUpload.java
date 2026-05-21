9622a274539b873eed4645a55587ef0dcdd588a8
bojjagoutham243-svg <bojja.goutham243@gmail.com>
2026-05-21 12:11:41 +0530

    Update CRM_IRF_Autofile_BulkUpload.java
    OnBoarding button Xpath Changed

--- CRM_IRF_Autofile_BulkUpload.java
+++ CRM_IRF_Autofile_BulkUpload.java
@@ -83,7 +83,7 @@ public class CRM_IRF_Autofile_BulkUpload {
             // ================= NAVIGATION =================
             WebElement onBoardMenu = wait.until(
                     ExpectedConditions.elementToBeClickable(
-                            By.xpath("//*[contains(text(),'On Board')]")
+                            By.xpath("(//p[normalize-space()='On Boardings'])[1]")
                     )
             );
             onBoardMenu.click();
